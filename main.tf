46e479d8ae2018cce7ab3755776803498556eef2
KANGJUHYEON <alinos@naver.com>
2023-07-06 19:42:54 +0900

    delete aws-instance

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-06b09bfacae1453cb"
-  instance_type = "t2.micro"
+# resource "aws_instance" "example" {
+#   ami           = "ami-06b09bfacae1453cb"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "terraform-example"
-  }
-}
+#   tags = {
+#     Name = "terraform-example"
+#   }
+# }
