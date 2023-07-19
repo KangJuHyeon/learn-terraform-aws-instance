b9950c38f4fea7e2b55cbea4a3201ef74fc2b745
KANGJUHYEON <alinos@naver.com>
2023-07-19 20:12:35 +0900

    웹 서버 클러스터 구성하기 실습

--- main.tf
+++ main.tf
@@ -2,60 +2,62 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# resource "aws_launch_configuration" "example" {
-#   image_id               = "ami-06ca3ca175f37dd66"
-#   instance_type          = "t2.micro"
-#   security_groups        = ["${aws_security_group.instance.id}"]
-  
-#   user_data =  <<-EOF
-#               #!/bin/bash
-#               echo "Hello, World" > index.html
-#               nohup /usr/bin/python3 -m http.server "${var.server_port}" &
-#               EOF
-  
-#   lifecycle  {
-#     create_before_destroy = true
-#   }
-# }
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-06ca3ca175f37dd66"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
 
-# resource "aws_security_group" "instance" {
-#   name = "terraform-example-instance"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup /usr/bin/python3 -m http.server "${var.server_port}" &
+              EOF
 
-#   ingress {
-#     from_port   = "${var.server_port}"
-#     to_port     = "${var.server_port}"
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
 
-# resource "aws_autoscaling_group" "example" {
-#   launch_configuration = "${aws_launch_configuration.example.id}"
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   min_size = 2
-#   max_size = 10
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-#   tag {
-#     key                 = "Name"
-#     value               = "terraform-asg-example"
-#     propagate_at_launch = true
-#   }
-# }
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.id
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
-# data "aws_vpc" "default" {
-#   default = true
-# }
+  min_size = 2
+  max_size = 10
 
-# data "aws_subnet_ids" "default" {
-#     vpc_id = "${data.aws_vpc.default.id}"
-# }
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
 
-# variable "server_port" {
-#     description = "The port the server will user for HTTP requests"
-#     type        = number
-#     default     = 8080
-# }
+# VPC 설정
+data "aws_subnet_ids" "default" {      
+    vpc_id = data.aws_vpc.default.id   
+}                                      
+
+data "aws_vpc" "default" {             
+    default = true                     
+}        
+
+variable "server_port" {
+  description = "The port the server will user for HTTP requests"
+  type        = number
+  default     = 8080
+}
